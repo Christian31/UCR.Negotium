1404033ff3e5f3c0d6b86d5210812d98d95ba74e
Christian Delgado <crikkk@outlook.com>
2019-05-14 00:00:58 -0600

    Se actualiza el trace del proyecto, pendiente analizar cómo dar permisos de escritura a la App

--- UCR.Negotium.Base/Trace/TraceException.cs
+++ UCR.Negotium.Base/Trace/TraceException.cs
@@ -1,5 +1,5 @@
-﻿using NLog;
-using System;
+﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace UCR.Negotium.Base.Trace
@@ -8,13 +8,42 @@ namespace UCR.Negotium.Base.Trace
     {
         public static void TraceExceptionAsync(this Exception exception)
         {
-            Task.Run(() => TraceLog(exception));
+            Task.Run(() => TraceExcepcion(exception));
         }
 
-        private static async void TraceLog(Exception ex)
+        private static void TraceExcepcion(this Exception exception)
         {
-            Logger logger = LogManager.GetLogger("fileLogger");
-            logger.Error(ex, "Error Inesperado");
+            try
+            {
+                string laFechaActual = DateTime.Now.ToString();
+                string laRutaDelLog = ObtengaElLogDelDia();
+                EscribaMensaje(laRutaDelLog, laFechaActual, exception.Message, exception.ToString());
+            }
+            catch(Exception ex)
+            {
+                //se omite lanzar la excepción
+            }
         }
+
+        private static void EscribaMensaje(string path, string fechaDelMensaje, string mensaje, string detalle)
+        {
+            string nuevoMensaje = string.Format(templateMessages, fechaDelMensaje, mensaje, detalle) + separadorDeMensajes;
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.Write(nuevoMensaje);
+            }
+        }
+
+        private static string ObtengaElLogDelDia()
+        {
+            string logPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Logs");
+            string nombreDelLog = $"mensajes-errores.log";
+            string fullLogPath = Path.Combine(logPath, nombreDelLog);
+
+            return fullLogPath;
+        }
+
+        private const string templateMessages = "Fecha de error: {0}, \n Mensaje: {1}, \n Detalle: {2}. \n";
+        private const string separadorDeMensajes = "/*****************************************************/ \n";
     }
 }
